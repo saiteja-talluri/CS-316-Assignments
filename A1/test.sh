6d67bb614851001f2bd715a7edeb01d4b4f6f9c6
TALLURI SAI TEJA <saitejat@sl2-38.cse.iitb.ac.in>
2019-01-23 15:35:13 +0530

    Final Submission

--- A1/test.sh
+++ A1/test.sh
@@ -17,10 +17,16 @@ for filename in "$testdir"/*.c; do
 	DIFF1=$(diff "$filename.toks" "$filename.mytoks")
 
 
-	./mysclp -eval "$filename"
+	./mysclp -eval "$filename" 2> /dev/null
 	mv "$filename.eval" "$filename.myeval"
-	./sclp -eval "$filename"
+	./sclp -eval "$filename" 2> /dev/null
 	DIFF2=$(diff "$filename.eval" "$filename.myeval")
+
+	./mysclp "$filename" 2>"$filename.myerr"
+	mv "$filename.spim" "$filename.myspim"
+	./sclp "$filename" 2>"$filename.err"
+	DIFF3=$(diff "$filename.spim" "$filename.myspim")
+	DIFF4=$(diff "$filename.err" "$filename.myerr")
 
 	if [ "$DIFF1" != "" ]
 	then
@@ -34,7 +40,23 @@ for filename in "$testdir"/*.c; do
 	then
 		echo "-eval failed on $filename. Did not run further tests (lexicographic ordering)."
 		echo "Showing diff:"
-		echo "$DIFF1"
+		echo "$DIFF2"
+		exit
+	fi
+
+	if [ "$DIFF3" != "" ]
+	then
+		echo "spimcode failed on $filename. Did not run further tests (lexicographic ordering)."
+		echo "Showing diff:"
+		echo "$DIFF3"
+		exit
+	fi
+
+	if [ "$DIFF4" != "" ]
+	then
+		echo "spim error message failed on $filename. Did not run further tests (lexicographic ordering)."
+		echo "Showing diff:"
+		echo "$DIFF4"
 		exit
 	fi
 done
